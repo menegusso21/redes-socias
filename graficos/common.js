12faf3d5916d98834dc4c74dc18cf02908864bc8
TANIA MENEGUSSO <110191073+menegusso21@users.noreply.github.com>
2024-10-24 16:05:37 -0300

    nnmnm

--- graficos/common.js
+++ graficos/common.js
@@ -1,12 +1,33 @@
-const getCSS = (variavel) => {
-    const bodyStyles = getComputedStyle(document.body)
-    return bodyStyles.getPropertyValue(variavel)
+import { getCSS } from "./common.js"
+
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
+
+    const data = [
+        {
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color')
+
+    }
+
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
 }
 
-const tickConfig = {
-    family: getCSS('--font'),
-    size: 16,
-    color: getCSS('--primary-color')
-}
-
-export {getCSS, tickConfig}
+quantidadeUsuariosPorRede()
